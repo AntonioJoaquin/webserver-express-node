ccccd9a312faadc3a8124351d575532efbcf3012
Antonio Joaquín <antoniojoaquinluque04@gmail.com>
2020-07-13 23:30:30 +0200

    Primer commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+// Express HBS Engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'antonio joaquin',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando las peticiones en el puerto ${port}`);
+});
